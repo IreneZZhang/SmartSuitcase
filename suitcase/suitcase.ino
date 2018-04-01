57c3ef7abe44222faf255f1b1efd08f403d78503
IreneZZhang <zhangzhiyang1002@gmail.com>
2018-04-01 16:00:02 -0500

    function prototypes

---
+++ suitcase/suitcase.ino
@@ -0,0 +1,51 @@
+// Imports
+#include <Wire.h>
+#include <BlynkSimpleSerialBLE.h>
+
+
+// Global variables 
+
+
+// Functions 
+/*
+ * Requires: nothing
+ * Effect: read the GPS coordinates from GPS sensor 
+ */
+void *getGPS() {
+  }
+
+
+/*
+ * 
+ */
+void setSpeedMotorA(int speed)
+{
+  }
+
+
+void setSpeedMotorB(int spped)
+{
+  }
+
+
+/*
+ * Requires:
+ *    speed: an int from ??? 
+ * Effect: 
+ *    Set the motor speed
+ */
+void setMotorSpeed(int speed)
+{
+  }
+
+  
+void setup()
+{
+  // Set up motor pins
+
+  // Bluethooth setup 
+  
+  
+  }
+
+
---
+++ suitcase_constants.h
@@ -0,0 +1,30 @@
+// Blynk Authentication 
+char auth[]  = "";
+
+// Pin Variables 
+#define BLE_TX_PIN 
+#define BLE_RX_PIN
+
+#define MOTOR_A_EN_PIN 
+#define MOTOR_B_EN_PIN 
+#define MOTOR_A_IN_1_PIN 
+#define MOTOR_A_IN_2_PIN 
+#define MOTOR_B_IN_1_PIN 
+#define MOTOR_B_IN_2_PIN 
+
+// If one motor tens to spin faster than the other, add offset
+#define MOTOR_A_OFFSET
+#define MOTOR_B_OFFSET
+
+// Declination Angle to compass
+#define DECLINATION_ANGLE 
+
+#define COMPASS_OFFSET
+
+// Update frequency of GPS
+#define GPS_UPDATE_INTERVAL
+
+// Timeout time 
+#define GPS_STREAM_TIMEOUT
+
+
